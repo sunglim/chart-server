29dd9c10e6fa3986b9850f92778e871ed9b8b52d
Sungguk Lim <sungguk.lim@sap.com>
2024-01-25 14:19:34 +0900

    Move to pkg

--- cmd/chart-server/chart-server.go
+++ cmd/chart-server/chart-server.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/sunglim/chart-server/internal/database"
-	"github.com/sunglim/chart-server/internal/server"
-)
+import "github.com/sunglim/chart-server/pkg/controller/chartserver"
 
 func main() {
-	err := database.CreateDatabase()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Print("Database initialized")
-
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		err := server.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	log.Print("Server started")
-
-	<-done
-	log.Print("Server Stopped")
+	chartserver.RunOrDie()
 }
